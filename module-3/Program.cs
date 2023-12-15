c59fd43b977f80f4fecdb1d09de404c9f778879c
imkoolya <imkoolya@gmail.com>
2023-12-15 20:49:04 +0300

    changed code

--- module-3/Program.cs
+++ module-3/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
         Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
+        var name = Console.ReadLine();
         Console.Write("Enter your age: ");
-        byte age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        var age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);      
+        Console.Write("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate: {0}", birthdate);
         Console.Write("What is your favorite day of week? ");
-
         weekdays day = (weekdays)int.Parse(Console.ReadLine());
-
         Console.WriteLine("Your favorite day is {0}", day);
         Console.ReadKey();
     }
@@ -25,6 +30,5 @@ class MainClass
         saturday = 5,
         sunday = 6,
     }
-
 }
 
